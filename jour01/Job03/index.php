7ca6353fa04ceab2eac3ddcc20c83f061389655f
DOTTO-9327 <hove-lesli-montana.dotto@laplateforme.io>
2025-07-11 16:31:27 +0200

    Job03 creation table begging done

---
+++ jour01/Job03/index.php
@@ -0,0 +1,58 @@
+<?php
+    $booleen= true;
+
+    $age= 30; 
+
+    $titre= "LaPlateforme";
+
+    $float= 10.10;
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Job03</title>
+</head>
+
+<body>
+    <table>
+        <thead>
+            <tr>
+                <td>Type</td>
+                <td>Nom</td>
+                <td>Valeur</td>
+            </tr>
+        </thead>
+            
+        <tbody>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+        </tbody>
+    </table>
+    
+</body>
+</html>
