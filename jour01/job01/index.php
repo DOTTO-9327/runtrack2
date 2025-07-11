953ba63c9e8db5695dc22f82ff3cbbabec6906cf
DOTTO-9327 <hove-lesli-montana.dotto@laplateforme.io>
2025-07-11 11:45:40 +0200

    Job 01 done

---
+++ jour01/job01/index.php
@@ -0,0 +1,14 @@
+<?php
+    $str= "LaPlateforme";
+    $str2= "Vive";
+    $str3= "!";
+    echo "$str2 $str $str3";
+
+    $val= 6+4;
+    echo "$val";
+
+    $myBool= true;
+    $myBool= false;
+    echo "$myBool";
+
+?>
