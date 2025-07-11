333df98ff96270b19d7fc53119bf263752970884
DOTTO-9327 <hove-lesli-montana.dotto@laplateforme.io>
2025-07-11 13:24:27 +0200

    Job 01 modify done

--- jour01/job01/index.php
+++ jour01/job01/index.php
@@ -4,10 +4,15 @@
     $str3= "!";
     echo "$str2 $str $str3";
 
-    $val= 6+4;
+    $val= 6;
+    echo "$val";
+
+    $val= $val+4;
     echo "$val";
 
     $myBool= true;
+    echo "$myBool";
+
     $myBool= false;
     echo "$myBool";
 
