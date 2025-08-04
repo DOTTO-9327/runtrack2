cf6ecd168dfab28b981d820e8dbc3f3b1965ba58
DOTTO-9327 <hove-lesli-montana.dotto@laplateforme.io>
2025-08-04 13:53:53 +0200

    jour03 job07 end

---
+++ jour03/job07/index.php
@@ -0,0 +1,36 @@
+<?php
+//Créez une variable de type string nommée $str et affectez y le texte :
+//“Certaines choses changent, et d'autres ne changeront jamais.”
+//Créer un algorithme qui parcourt cette string en remplaçant le premier caractère par le deuxième, le deuxième par le troisième etc.. et le dernier par le premier.
+// Ex. : Ertaines choses changent, et d'autres ne changeront jamais.c
+
+
+// qu'est ce qu'on nous demande de faire ? 
+// On nous demande de bouger chaque lettre d’un cran vers la gauche: La 2ème lettre va à la place de la 1ère, la 3ème lettre va à la place de la 2ème, la 4ème va à la place de la 3ème..., et la dernière lettre (le ".") doit aller à la 1ère place. Comme un cercle qui tourne. 
+
+
+
+
+$str = "Certaines choses changent, et d'autres ne changeront jamais."; 
+
+$newStr = ""; // on crée une nouvelle chaîne vide
+
+for ($i = 0; isset($str[$i + 1]); $i++) //  Cette boucle for dit : Commence à la position 0.Tant qu’il existe un caractère à la position $i + 1, continue. Avance de 1 à chaque tour On utilise isset($str[$i + 1]) pour vérifier si le caractère suivant existe (comme demander "Est-ce qu'il y a encore une lettre après celle-ci ?").
+ 
+
+{
+    $newStr =  $newStr.$str[$i + 1]; // on ajoute la lettre suivante à la nouvelle chaîne
+}
+
+// À la fin, on ajoute la première lettre à la fin de la chaîne
+$newStr = $newStr.$str[0];
+
+echo $newStr;
+
+
+
+
+
+
+
+?>
